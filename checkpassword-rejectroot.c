7722f1c22227960d63ad5cd352c0dfbf5c657497
Amitai Schleier <schmonz-web-git@schmonz.com>
2017-10-06 01:15:31 -0400

    checkpassword-rejectroot: log root attempts.

--- checkpassword-rejectroot.c
+++ checkpassword-rejectroot.c
@@ -5,15 +5,15 @@
 char sserrbuf[128];
 substdio sserr = SUBSTDIO_FDBUF(write,2,sserrbuf,sizeof sserrbuf);
 
-void err(char *s) {
+void errflush(char *s) {
   substdio_puts(&sserr,s);
   substdio_puts(&sserr,"\n");
   substdio_flush(&sserr);
 }
 
 void die() { _exit(1); }
-void die_usage() { err("usage: checkpassword-rejectroot prog"); die(); }
-void die_root() { err("checkpassword-rejectroot: am root"); die(); }
+void die_usage() { errflush("usage: checkpassword-rejectroot prog"); die(); }
+void die_root() { errflush("checkpassword-rejectroot: am root"); die(); }
 
 int main(int argc,char **argv) {
   char **childargs;
@@ -21,7 +21,10 @@ int main(int argc,char **argv) {
   childargs = argv + 1;
   if (!*childargs) die_usage();
 
-  if (getuid() == 0) die_root();
+  if (getuid() == 0) {
+    errflush("checkpassword-rejectroot: ROOT LOGIN ATTEMPTED");
+    die_root();
+  }
  
   execvp(*childargs,childargs);
   die();
