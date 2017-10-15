fd91ed3c98d4e6d9761c004e79c820c9fae18643
Amitai Schleier <schmonz-web-git@schmonz.com>
2017-10-14 23:37:50 -0400

    Extract assert_config_parse() and use it again.

--- qmail-fixsmtpio-tests.c
+++ qmail-fixsmtpio-tests.c
@@ -2,29 +2,53 @@
 
 #include "qmail-fixsmtpio.h"
 
-void test_parse_config_clienteof(void) {
+void assert_config_parse(char *line,
+                         char *env,char *event,char *request_prepend,
+                         char *response_line_glob,char *exitcode_str,
+                         char *response) {
   stralloc config          = {0};
   int pos                  = 0;
 
-  copys(&config,"AUTHUSER:clienteof::*:0");
+  copys(&config,line);
 
-  char *env                = get_next_field(&pos,&config);
-  char *event              = get_next_field(&pos,&config);
-  char *request_prepend    = get_next_field(&pos,&config);
-  char *response_line_glob = get_next_field(&pos,&config);
-  char *exitcode_str       = get_next_field(&pos,&config);
-  char *response           = get_next_field(&pos,&config);
+  char *actual_env                = get_next_field(&pos,&config);
+  char *actual_event              = get_next_field(&pos,&config);
+  char *actual_request_prepend    = get_next_field(&pos,&config);
+  char *actual_response_line_glob = get_next_field(&pos,&config);
+  char *actual_exitcode_str       = get_next_field(&pos,&config);
+  char *actual_response           = get_next_field(&pos,&config);
 
-  TEST_ASSERT_EQUAL_STRING( "AUTHUSER", env);
-  TEST_ASSERT_EQUAL_STRING("clienteof", event);
-  TEST_ASSERT_EQUAL_STRING(         "", request_prepend);
-  TEST_ASSERT_EQUAL_STRING(        "*", response_line_glob);
-  TEST_ASSERT_EQUAL_STRING(        "0", exitcode_str);
-  TEST_ASSERT_NULL(	                    response);
+  TEST_ASSERT_EQUAL_STRING(env, actual_env);
+  TEST_ASSERT_EQUAL_STRING(event, actual_event);
+  TEST_ASSERT_EQUAL_STRING(request_prepend, actual_request_prepend);
+  TEST_ASSERT_EQUAL_STRING(response_line_glob, actual_response_line_glob);
+  TEST_ASSERT_EQUAL_STRING(exitcode_str, actual_exitcode_str);
+  TEST_ASSERT_EQUAL_STRING(response, actual_response);
+}
+
+void test_parse_config_clienteof(void) {
+  assert_config_parse("AUTHUSER:clienteof::*:0",
+                      "AUTHUSER",
+                      "clienteof",
+                      "",
+                      "*",
+                      "0",
+                      NULL);
+}
+
+void test_parse_config_greeting4xx(void) {
+  assert_config_parse("AUTHUSER:greeting::4*:14",
+                      "AUTHUSER",
+                      "greeting",
+                      "",
+                      "4*",
+                      "14",
+                      NULL);
 }
 
 int main(void) {
   UNITY_BEGIN();
   RUN_TEST(test_parse_config_clienteof);
+  RUN_TEST(test_parse_config_greeting4xx);
   return UNITY_END();
 }
