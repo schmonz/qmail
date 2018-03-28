c3c1ac618c873f00e1fa83f7de4de5f92971fde8
Amitai Schleier <schmonz-web-git@schmonz.com>
2018-03-28 13:59:31 -0600

    CppUTest ain't gonna work with this old C code.

--- qmail-fixsmtpio-tests.cpp
+++ qmail-fixsmtpio-tests.cpp
@@ -1,13 +1,34 @@
 #include "CppUTest/CommandLineTestRunner.h"
 #include "CppUTest/TestHarness.h"
 
-TEST_GROUP(FirstTestGroup)
+extern "C" {
+    #include "qmail-fixsmtpio-filter.h"
+    #include "stralloc.h"
+}
+
+TEST_GROUP(FilterStuff)
 {
 };
 
-TEST(FirstTestGroup, FirstTest)
+TEST(FilterStuff, StripLastEol)
 {
-    LONGS_EQUAL(7, 7);
+    stralloc sa = {0};
+
+    LONGS_EQUAL(0, sa.s);
+    LONGS_EQUAL(0, sa.len);
+    strip_last_eol(&sa);
+    LONGS_EQUAL(0, sa.s);
+    LONGS_EQUAL(0, sa.len);
+/*
+ * given empty stralloc, return empty stralloc
+ * given stralloc consisting of "\n", return empty stralloc
+ * given stralloc consisting of "\r", return empty stralloc
+ * given stralloc consisting of "\r\n", return empty stralloc
+ * given stralloc consisting of "\n\r", ???
+ * given stralloc consisting of "yo geeps", return "yo geeps"
+ * given stralloc consisting of "yo geeps\r\n", return "yo geeps"
+ * given stralloc consisting of "yo geeps\r\nhow you doin?\r\n", return "yo geeps\r\nhow you doin?"
+ */
 }
 
 int main(int ac, char** av)
