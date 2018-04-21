451940f425feb8f391a35d479a4a71e39c0bb865
Amitai Schleier <schmonz-web-git@schmonz.com>
2018-04-20 21:18:26 -0400

    Make an assertion a normal person would make.

--- qmail-fixsmtpio-tests.c
+++ qmail-fixsmtpio-tests.c
@@ -4,28 +4,30 @@
 #include "qmail-fixsmtpio-filter.h"
 #include "stralloc.h"
 
-void assert_strip_last_eol(int charsremoved, const char *input) {
+void assert_strip_last_eol(const char *input, const char *expected_output) {
   stralloc sa = {0};
   stralloc_copys(&sa, input);
-  int len = sa.len;
 
   strip_last_eol(&sa);
 
-  ck_assert_int_eq(len - charsremoved, sa.len);
+  ck_assert_int_eq(sa.len, strlen(expected_output));
+  stralloc_0(&sa);
+  ck_assert_str_eq(sa.s, expected_output);
 }
 
 START_TEST (test_strip_last_eol)
 {
-  assert_strip_last_eol(1, "\n");
-  assert_strip_last_eol(1, "\r");
-  assert_strip_last_eol(2, "\r\n");
-  assert_strip_last_eol(1, "\n\r");
-  assert_strip_last_eol(1, "\r\r");
-  assert_strip_last_eol(1, "\n\n");
-  assert_strip_last_eol(0, "yo geeps");
-  assert_strip_last_eol(2, "yo geeps\r\n");
-  assert_strip_last_eol(2, "yo geeps\r\nhow you doin?\r\n");
-  assert_strip_last_eol(0, "yo geeps\r\nhow you doin?");
+  assert_strip_last_eol("", "");
+  assert_strip_last_eol("\n", "");
+  assert_strip_last_eol("\r", "");
+  assert_strip_last_eol("\r\n", "");
+  assert_strip_last_eol("\n\r", "\n");
+  assert_strip_last_eol("\r\r", "\r");
+  assert_strip_last_eol("\n\n", "\n");
+  assert_strip_last_eol("yo geeps", "yo geeps");
+  assert_strip_last_eol("yo geeps\r\n", "yo geeps");
+  assert_strip_last_eol("yo geeps\r\nhow you doin?\r\n", "yo geeps\r\nhow you doin?");
+  assert_strip_last_eol("yo geeps\r\nhow you doin?", "yo geeps\r\nhow you doin?");
 }
 END_TEST
 
