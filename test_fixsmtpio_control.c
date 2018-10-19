810e39981284b808928d41a180ca9b39417f38d5
Amitai Schleier <schmonz-web-git@schmonz.com>
2018-10-19 16:16:42 -0400

    Default exitcode is EXIT_LATER_NORMALLY.

--- test_fixsmtpio_control.c
+++ test_fixsmtpio_control.c
@@ -28,7 +28,7 @@ filter_rule *parse_control_line(stralloc *line) {
   rule->event               = NULL;
   rule->request_prepend     = NULL;
   rule->response_line_glob  = NULL;
-  rule->exitcode            = 0;
+  rule->exitcode            = EXIT_LATER_NORMALLY;
   rule->response            = NULL;
 
   stralloc value = {0}; stralloc_copys(&value, "");
@@ -77,7 +77,7 @@ START_TEST (test_blank_line)
 {
   stralloc line = {0};
   filter_rule *rule = parse_control_line(&line);
-  assert_parsed_line(rule, NULL, NULL, NULL, NULL, 0, NULL);
+  assert_parsed_line(rule, NULL, NULL, NULL, NULL, EXIT_LATER_NORMALLY, NULL);
 }
 END_TEST
 
@@ -85,7 +85,7 @@ START_TEST (test_nonblank_line)
 {
   stralloc line = {0}; stralloc_copys(&line, ",");
   filter_rule *rule = parse_control_line(&line);
-  assert_parsed_line(rule, NULL, NULL, NULL, NULL, 0, NULL);
+  assert_parsed_line(rule, NULL, NULL, NULL, NULL, EXIT_LATER_NORMALLY, NULL);
 }
 END_TEST
 
@@ -93,7 +93,7 @@ START_TEST (test_no_env_or_event)
 {
   stralloc line = {0}; stralloc_copys(&line, ":");
   filter_rule *rule = parse_control_line(&line);
-  assert_parsed_line(rule, NULL, NULL, NULL, NULL, 0, NULL);
+  assert_parsed_line(rule, NULL, NULL, NULL, NULL, EXIT_LATER_NORMALLY, NULL);
 }
 END_TEST
 
@@ -101,7 +101,7 @@ START_TEST (test_no_env_yes_event)
 {
   stralloc line = {0}; stralloc_copys(&line, ":smtp_verb");
   filter_rule *rule = parse_control_line(&line);
-  assert_parsed_line(rule, NULL, "smtp_verb", NULL, NULL, 0, NULL);
+  assert_parsed_line(rule, NULL, "smtp_verb", NULL, NULL, EXIT_LATER_NORMALLY, NULL);
 }
 END_TEST
 
@@ -109,7 +109,7 @@ START_TEST (test_yes_env_yes_event)
 {
   stralloc line = {0}; stralloc_copys(&line, "ENV_VAR:some_verb");
   filter_rule *rule = parse_control_line(&line);
-  assert_parsed_line(rule, "ENV_VAR", "some_verb", NULL, NULL, 0, NULL);
+  assert_parsed_line(rule, "ENV_VAR", "some_verb", NULL, NULL, EXIT_LATER_NORMALLY, NULL);
 }
 END_TEST
 
@@ -117,7 +117,7 @@ START_TEST (test_realistic_line)
 {
   stralloc line = {0}; stralloc_copys(&line, ":word:NOOP :*::250 indeed");
   filter_rule *rule = parse_control_line(&line);
-  assert_parsed_line(rule, NULL, "word", "NOOP ", "*", 0, "250 indeed");
+  assert_parsed_line(rule, NULL, "word", "NOOP ", "*", EXIT_LATER_NORMALLY, "250 indeed");
 }
 END_TEST
 
