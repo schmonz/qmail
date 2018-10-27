8dacf46ab423c2b8bec1b397b3346096405968e2
Amitai Schleier <schmonz-web-git@schmonz.com>
2018-10-27 10:15:42 -0400

    Replace dup2() with fd_copy().

--- authup.c
+++ authup.c
@@ -164,14 +164,14 @@ int starttls_init(void)
     return 0;
   if (!scan_ulong(fdstr,&fd))
     return 0;
-  if (dup2((int)fd,0) == -1)
+  if (fd_copy(0,(int)fd) == -1)
     return 0;
 
   if (!(fdstr=env_get("SSLWRITEFD")))
     return 0;
   if (!scan_ulong(fdstr,&fd))
     return 0;
-  if (dup2((int)fd,1) == -1)
+  if (fd_copy(1,(int)fd) == -1)
     return 0;
 
   return 1;
